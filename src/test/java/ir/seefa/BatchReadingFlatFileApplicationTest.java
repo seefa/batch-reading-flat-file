a7191b90bb3523d1547013ba19bc354db2dd152d
sami <sami.delfan@gmail.com>
2022-07-30 06:00:42 +0430

    add test spring loader

---
+++ src/test/java/ir/seefa/BatchReadingFlatFileApplicationTest.java
@@ -0,0 +1,16 @@
+package ir.seefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Saman Delfani
+ * @version 1.0
+ * @since 2022-07-30 05:59:57
+ */
+@SpringBootTest
+public class BatchReadingFlatFileApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
